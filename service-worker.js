ceab58a028037fd92d50ef854211dba0bfe6731f
seb <svenkarlsebastian@gmail.com>
2024-03-07 16:22:07 +0100

    add

--- service-worker.js
+++ service-worker.js
@@ -1,77 +1,15 @@
-// Service worker script
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open("v1");
+  await cache.addAll(resources);
+};
 
-// Define the cache name
-const CACHE_NAME = 'my-cache';
-
-// List of URLs to cache
-const urlsToCache = [
-  '/index.html',
-  '/style.css',
-  '/img/logo.png',
-  '/img/logo2.png',
-  '/img/logo_symbol.png'
-];
-
-// Install event: Cache resources
-self.addEventListener('install', function(event) {
-  // Perform install steps
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+    addResourcesToCache([
+      "/",
+      "/index.html",
+      "/style.css",
 
-// Fetch event: Serve from cache or fetch from network
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // Clone the request
-        let fetchRequest = event.request.clone();
-
-        // Make network request
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if valid response
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // Clone response
-            let responseToCache = response.clone();
-
-            // Cache fetched resource
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
-        );
-      })
-    );
-});
-
-// Activate event: Clean up old caches
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    ]),
   );
 });
